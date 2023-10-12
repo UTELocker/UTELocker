07dd517386bd7fa420cf749c4dc1c502a8ba8e97
lviet <lviet456@gmail.com>
2023-10-12 09:11:50 +0700

    [NOTIFICATION] Migrate table

---
+++ database/migrations/2023_10_12_020559_create_notifications_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->id();
+            $table->string('parent_table')->nullable();
+            $table->integer('parent_id')->nullable();
+            $table->string('type');
+            $table->foreignId('client_id')->nullable()->constrained('clients');
+            $table->foreignId('owner_id')->nullable()->constrained('users');
+            $table->text('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('notifications');
+    }
+};
