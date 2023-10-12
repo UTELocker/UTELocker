30491f6748cfab8b03ccc22fbffcd3e037c0bb42
lviet <lviet456@gmail.com>
2023-10-12 12:14:00 +0700

    [GLOBAL_SETTINGS] Add settings pusher

---
+++ database/migrations/2023_10_12_050605_add_setting_pusher_to_global_settings_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('global_settings', function (Blueprint $table) {
+            $table->string('pusher_app_id')->nullable();
+            $table->string('pusher_app_key')->nullable();
+            $table->string('pusher_app_secret')->nullable();
+            $table->string('pusher_app_cluster')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('global_settings', function (Blueprint $table) {
+            //
+        });
+    }
+};
